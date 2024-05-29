e54cfe7e5116770568470691e71d11895eaec55d
Skaaan <skanderayari96@gmail.com>
2024-05-29 16:35:11 +0200

    Add OrderService and OrderController

---
+++ src/main/java/com/microservices/order/service/OrderService.java
@@ -0,0 +1,32 @@
+package com.microservices.order.service;
+
+import com.microservices.order.dto.OrderRequest;
+import com.microservices.order.model.Order;
+import com.microservices.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    private static Order mapToOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setAmount(orderRequest.quantity() );
+        return order;
+    }
+
+    public void createOrder(OrderRequest orderRequest) {
+        var order = mapToOrder(orderRequest);
+        orderRepository.save(order);
+    }
+
+}
